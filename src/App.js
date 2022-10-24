5d0745932083de27d52ae061505978b7cf6f5c02
chan0823 <a3327764@naver.com>
2022-10-24 15:28:51 +0900

    TEST 오민찬

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+import ExMain from './MainHome/Main';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          dsdas
-          Test -오민찬
-          Test - 이찬영
-        </a>
-      </header>
+    <div>
+      <ExMain/>
     </div>
   );
 }
