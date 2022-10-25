d1001841de19848f98347bffe8b65eae8fae3b50
chan0823 <a3327764@naver.com>
2022-10-25 17:48:44 +0900

    main 변경 -오민찬

--- src/MainHome/Main.js
+++ src/MainHome/Main.js
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 import "./Main.css";
 
 const Container = styled.div`
-    width: 1120px;
-    height: 630px;
-    border: 1px solid black;
-    margin: 10px;
+    width: 100%;
+    height: 100vh;
+    border: 1px solid none;
+    margin: 10px 0 0 0;
     padding: 0;
     display:flex ;
     justify-content:center;
@@ -16,8 +16,8 @@ const Container = styled.div`
 `;
 const LeftMain = styled.div`
     display: flex;
-    width: 560px;
-    height: 630px;
+    width: 50%;
+    height: 100vh;
     /* border: 1px solid none; */
     box-sizing: border-box;
     margin: 0 auto;
@@ -40,8 +40,8 @@ const SubLogo = styled.div`
 `;
 const RightMain = styled.div`
     background-color: #4555AE;
-    width: 560px;
-    height: 630px;
+    width: 50%;
+    height: 100vh;
     border: 1px solid none;
     margin: auto;
     display: flex;
