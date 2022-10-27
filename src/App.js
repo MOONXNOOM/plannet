9d5c63df2f0775b25f5a73099e74e75dc7d7f7f9
chan0823 <a3327764@naver.com>
2022-10-27 16:29:56 +0900

    모달 변경 -오민찬

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/create" element={<Create />}/>
         <Route path="/doLogin" element={<DoLogin />}/>
         <Route path="/join" element={<Join />}/>
-        {/* <Route parh="/loginHome" element={<LoginHome />}/> */}
+        <Route parh="/loginHome" element={<LoginHome />}/>
       </Routes>
     </Router>
   );
