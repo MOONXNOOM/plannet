b02e956b40397791be7a36785d5a4cbf5a5d405f
lyb0623 <nnoonxnoonn@gmail.com>
2022-11-02 09:59:01 +0900

    BoardDetail 을 PostView로 변경

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Main from './Main/Main';
 import Home from './Home/Home';
 import Find from './Join/Find';
 import Create from './Board/Create';
-import BoardDetail from './Board/BoardDetail';
+import BoardDetail from './Board/PostView';
 
 function App() {
   return (
@@ -24,6 +24,8 @@ function App() {
         <Route path="/write" element={<Write />}/>
 
         <Route path="/board" element={<Board />}/>
+        <Route exact path='/postView/:no' component={PostView} />
+        <Route exact path='/' component={PostMain} />
         <Route path="/boardDetail" element={<BoardDetail />}/>
         <Route path="/create" element={<Create />}/>
       
