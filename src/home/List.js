cc819a64addc07cb322fbc8447f0d3e779a8e2b3
ksh0918 <kim.sehee.0918@gmail.com>
2022-11-03 17:15:34 +0900

    list css2

--- src/home/List.js
+++ src/home/List.js
@@ -1,10 +1,13 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import Api from "../api/plannetApi";
 
 const ListBox = styled.div`
     width: 100%;
-    ul:first-child p{color: rgb(206, 36, 36);}
-    ul:last-child p{color: #3249ce;}
-    ul{
+    div:first-child p{color: rgb(206, 36, 36);}
+    div:last-child p{color: #3249ce;}
+    div{
         display: block;
         width: calc((100% - 30px) / 7);
         height: 230px;
@@ -22,15 +25,42 @@ const ListBox = styled.div`
             margin-bottom: 7px;
             border-bottom: 2px solid #eee;
         }
-        li{
-            padding: 3px 8px;
-            white-space:nowrap;
+        ul{
+            height: 180px;
             overflow: hidden;
-            text-overflow: ellipsis;
-            list-style-type: disc !important;
-            list-style-position:inside;
-            marker-offset: 2px;
+            overflow-y: scroll;
+            padding: 0;
+            &:focus {outline: none;}
+            &::-webkit-scrollbar {
+                width: 5px;
+                padding: 5px;
+            }
+            &::-webkit-scrollbar-thumb {
+                height: 30%; /* 스크롤바의 길이 */
+                background: #ddd; /* 스크롤바의 색상 */
+                border-radius: 10px;
+                border: 0px solid transparent;
+                background-clip: padding-box;
+            }
+            &::-webkit-scrollbar-track {
+                background: none;
+                /*스크롤바 뒷 배경 색상*/
+            }
+            li{
+                padding: 3px 8px;
+                color: #4555ae;
+                white-space:nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                list-style-type: disc;
+                list-style-position: inside;
+                span{
+                    display: inline;
+                    margin-left: -8px;
+                }
+            }
         }
+        
         &:last-child{
             margin-right: 0;
         }
@@ -38,50 +68,107 @@ const ListBox = styled.div`
 `;
 
 const List = () => {
+    const [sunList, setSunList] = useState([]);
+    const [monList, setMonList] = useState([]);
+    const [tueList, setTueList] = useState([]);
+    const [wedList, setWedList] = useState([]);
+    const [thuList, setThuList] = useState([]);
+    const [friList, setFriList] = useState([]);
+    const [satList, setSatList] = useState([]);
+
+    const getId = window.localStorage.getItem("userId");
+    useEffect(() => {
+      const planLoad = async() => {
+          try{
+                const response = await Api.weekList(getId);
+                console.log(response.data);
+                setSunList();
+                setMonList();
+                setTueList();
+                setWedList();
+                setThuList();
+                setFriList();
+                setSatList();
+          } catch(e){
+              console.log(e);
+          }
+      }
+      planLoad();
+  },[getId]);
     return(
         <ListBox>
-            <ul>
+            <div>
                 <p>SUN</p>
-                <li>일정1</li>
-                <li>일정1</li>
-                <li>일정1</li>
-            </ul>
-            <ul>
+                <ul>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                </ul>
+            </div>
+            <div>
                 <p>MON</p>
-                <li>일정11111111111111111111111</li>
-                <li>일정1</li>
-                <li>일정1</li>
-            </ul>
-            <ul>
+                <ul>
+                    <li><span>일정111111111</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                </ul>
+            </div>
+            <div>
                 <p>TUE</p>
-                <li>일정1</li>
-                <li>일정1</li>
-                <li>일정1</li>
-            </ul>
-            <ul>
+                <ul>
+                    <li><span>일정111111111</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                </ul>
+            </div>
+            <div>
                 <p>WED</p>
-                <li>일정1</li>
-                <li>일정1</li>
-                <li>일정1</li>
-            </ul>
-            <ul>
+                <ul>
+                    <li><span>일정111111111</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                </ul>
+            </div>
+            <div>
                 <p>THU</p>
-                <li>일정1</li>
-                <li>일정1</li>
-                <li>일정1</li>
-            </ul>
-            <ul>
+                <ul>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                </ul>
+            </div>
+            <div>
                 <p>FRI</p>
-                <li>일정1</li>
-                <li>일정1</li>
-                <li>일정1</li>
-            </ul>
-            <ul>
+                <ul>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                </ul>
+            </div>
+            <div>
                 <p>SAT</p>
-                <li>일정1</li>
-                <li>일정1</li>
-                <li>일정1</li>
-            </ul>
+                <ul>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                    <li><span>일정1</span></li>
+                </ul>
+            </div>
             
         </ListBox>
     );
