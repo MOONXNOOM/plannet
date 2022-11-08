bf540e3d5d9d4409daffce7eff28815c03093951
lyb0623 <nnoonxnoonn@gmail.com>
2022-11-08 17:03:39 +0900

    Lib 수정

--- src/Lib/PrivateRoute.js
+++ src/Lib/PrivateRoute.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-
 const PrivateRoute = ({ children }) => {
     const isLogin = window.localStorage.getItem("isLogin");
     if(isLogin !== "true") alert("로그인 후 이용해주세요.");
 
-  return isLogin === "true" ? children : <Navigate to="/"/>;
+    return isLogin === "true" ? children : <Navigate to="/"/>;
 };
 
 export default PrivateRoute;
