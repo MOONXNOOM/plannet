47c8992c72b3a0c382f2c4deb0f88c7e8a574fce
lyb0623 <nnoonxnoonn@gmail.com>
2022-11-08 16:36:22 +0900

    api 포함 기본 베이스 코드들 정리 및 수정

--- src/App.js
+++ src/App.js
@@ -1,51 +1,37 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Write from './Write/Write';
-import Board from './Board/Board';
+import Main from './Main/Main';
 import DoLogin from './DoLogin/DoLogin';
 import Join from './Join/Join';
-import Main from './Main/Main';
-import Home from './Home/Home';
 import Find from './Join/Find';
-import Create from './Board/Create';
 import Setting from './Setting/Setting';
+import Home from './Home/Home';
+import Write from './Write/Write';
+import Board from './Board/Board';
+import Create from './Board/Create';
+import Edit from './Board/Edit';
 import PostView from './Board/PostView';
 import PublicRoute from './Lib/PublicRoute';
 import PrivateRoute from './Lib/PrivateRoute';
-import Edit from './Board/Edit';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<PublicRoute><Main /></PublicRoute>}/>
-        <Route path='/doLogin' element={<PublicRoute><DoLogin /></PublicRoute>}/>
-        <Route path='/join' element={<PublicRoute><Join /></PublicRoute>}/>
-        <Route path='/find' element={<PublicRoute><Find /></PublicRoute>}/>
-
-        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>}/>
-        <Route path="/write/:date" element={<PrivateRoute><Write /></PrivateRoute>}/>
-        <Route path="/setting" element={<PrivateRoute><Setting /></PrivateRoute>}/>
-        <Route path="/board" element={<PrivateRoute><Board /></PrivateRoute>}/>
-        <Route exact path='/postView/:no' element={<PrivateRoute><PostView /></PrivateRoute>}/>
-        <Route path="/create" element={<PrivateRoute><Create /></PrivateRoute>}/>
-        <Route path="/write/:date" element={<PrivateRoute><Write /></PrivateRoute>}/>
-        <Route path="/edit/:no" element={<PrivateRoute><Edit /></PrivateRoute>}/>
-
-        {/* <Route path="/" element={<Main />}/>
-        <Route path="/doLogin" element={<DoLogin />}/>
-        <Route path="/join" element={<Join />}/>
-        <Route path="/find" element={<Find />}/>
-
-        <Route path="/home" element={<Home />}/>
-        <Route exact path='/write/:date' element={<Write />} />
-        <Route path="/setting" element={<Setting />}/>
-        <Route path="/board" element={<Board />}/>
-        <Route exact path='/postView/:no' element={<PostView />}/>
-        <Route path="/create" element={<Create />}/> */}
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path='/' element={<PublicRoute><Main /></PublicRoute>}/>
+                <Route path='/doLogin' element={<PublicRoute><DoLogin /></PublicRoute>}/>
+                <Route path='/join' element={<PublicRoute><Join /></PublicRoute>}/>
+                <Route path='/find' element={<PublicRoute><Find /></PublicRoute>}/>
+                <Route path="/setting" element={<PrivateRoute><Setting /></PrivateRoute>}/>
+                <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>}/>
+                <Route path="/write/:date" element={<PrivateRoute><Write /></PrivateRoute>}/>
+                <Route path="/board" element={<PrivateRoute><Board /></PrivateRoute>}/>
+                <Route path="/create" element={<PrivateRoute><Create /></PrivateRoute>}/>
+                <Route path="/edit/:no" element={<PrivateRoute><Edit /></PrivateRoute>}/>
+                <Route exact path='/postView/:no' element={<PrivateRoute><PostView /></PrivateRoute>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
